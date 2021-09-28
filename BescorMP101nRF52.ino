56c6dd021526e0f765c52ecd278ce5d47243ca37
xchg-dot-ca <lytvynyuk@yahoo.com>
2021-09-28 11:16:16 -0400

    Code cleanup
    Removed unnecessary code and comments

--- BescorMP101nRF52.ino
+++ BescorMP101nRF52.ino
@@ -1,15 +1,14 @@
 /**
- * Besor MP 101 controller Arduino code
+ * Besor MP 101 controller Arduino code for nRF52 based dev board
 */
-
 #include <bluefruit.h>
 #include "cameraslider.h"
 #include <Adafruit_DotStar.h>
 
-//Use these pin definitions for the ItsyBitsy M4
+//Use these pin definitions for the ItsyBitsy M4 for LED
 #define DATAPIN    8
 #define CLOCKPIN   6
-#define NUMPIXELS 1
+#define NUMPIXELS  1
 
 Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
 
@@ -17,10 +16,6 @@ const int upPin =  10;
 const int downPin =  11;
 const int leftPin =  13;
 const int rightPin =  12;
-
-int headDirection = 0;
-int newCommand = -1;
-int currentCommand = -1;
 
 enum MoveValues {
   TILT_UP,
@@ -40,13 +35,16 @@ BLEUart bleuart;
 // Packet buffer
 extern uint8_t packetbuffer[];
 
+/**
+ *  Sets the apropriate i/o to move head
+ */
 void moveHead(int direction) {  
   switch(direction) {
     case TILT_UP:        digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,HIGH); digitalWrite(downPin,LOW); break;
     case TILT_DOWN:      digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,HIGH); break;
-    case PAN_LEFT:      digitalWrite(leftPin,HIGH); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
-    case PAN_RIGHT:     digitalWrite(leftPin,LOW); digitalWrite(rightPin,HIGH); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
-    case STOP:      digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
+    case PAN_LEFT:       digitalWrite(leftPin,HIGH); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
+    case PAN_RIGHT:      digitalWrite(leftPin,LOW); digitalWrite(rightPin,HIGH); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
+    case STOP:           digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
   }
 }
 
@@ -72,6 +70,9 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
   moveHead(STOP);
 }
 
+/**
+ * Intialise and started advertising
+ */
 void startAdv(void)
 {
   // Advertising packet
@@ -84,16 +85,7 @@ void startAdv(void)
   // Secondary Scan Response packet (optional)
   // Since there is no room for 'Name' in Advertising packet
   Bluefruit.ScanResponse.addName();
-
-  /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */ 
+  
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
@@ -125,6 +117,8 @@ void setup() {
 
   strip.begin(); // Initialize pins for output
   strip.show();  // Turn all LEDs off ASAP
+
+  // May not be required, currently used for debugging
   strip.setPixelColor(0, 255, 0, 0); strip.show(); // RED
 }
 
@@ -136,28 +130,5 @@ void loop() {
     char command = packetbuffer[0];
     Serial.print ("Command "); Serial.println(command, HEX);
     moveHead(command);
-  }
-  
-  /*
-  if(Serial.available()) {
-    newCommand = -1;
-    currentCommand = -1;
-  }
-
-  while(Serial.available()) {
-      newCommand = Serial.read();
-  }
-  
-  if(newCommand != currentCommand) {
-    currentCommand = newCommand;
-    moveHead(currentCommand);
-  }
-  */
-      
-  // Echo command back to Commander for DEBUGGING
-  /*
-  if (currentCommand != "") {
-    commandLabel.concat(currentCommand);
-    Serial.print(commandLabel);
-  }*/     
+  }    
 }
--- packetParser.cpp
+++ packetParser.cpp
@@ -2,8 +2,8 @@
 #include <bluefruit.h>
 #include "cameraslider.h"
 
-//    READ_BUFSIZE            Size of the read buffer for incoming packets
-#define READ_BUFSIZE                    (20)
+// Size of the read buffer for incoming packets
+#define READ_BUFSIZE (20)
 
 /* Buffer to hold incoming characters */
 uint8_t packetbuffer[READ_BUFSIZE];
@@ -34,20 +34,4 @@ uint8_t readPacketNew(BLEUart *ble_uart, uint16_t timeout)
     return 0;
 
   return replyidx;
-}
-
-uint8_t writePacket (BLEUart *ble_uart, char* writeBuff, int sizeOfBuff) {
-  ble_uart->write(writeBuff, sizeOfBuff);
-}
-
-bool parseStartPayload (startPacketPayload *payload) {
-
-  if(packetbuffer[0] == STATUS_COMMAND) {
-    payload->startPosition = int(packetbuffer[1] << 24 | packetbuffer[2] << 16 | packetbuffer[3] << 8 | packetbuffer[4]);
-    payload->stopPosition = int(packetbuffer[5] << 24 | packetbuffer[6] << 16 | packetbuffer[7] << 8 | packetbuffer[8]);
-    payload->timeToRun = int(packetbuffer[9] << 24 | packetbuffer[10] << 16 | packetbuffer[11] << 8 | packetbuffer[12]);
-    return true;
-  } else {
-    return false;
-  }
 }

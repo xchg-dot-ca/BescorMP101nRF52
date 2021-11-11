a9987cb2eae8d8a3b03b2e85f4ee455c82ee89b0
xchg-dot-ca <lytvynyuk@yahoo.com>
2021-11-11 14:45:03 -0500

    Update BescorMP101nRF52.ino
    Added LED indication according to the Head state
    Cleaned up the code

--- BescorMP101nRF52.ino
+++ BescorMP101nRF52.ino
@@ -1,5 +1,7 @@
 /**
  * Besor MP 101 controller Arduino code for nRF52 based dev board
+ * Developed by Inside Out Electronics
+ * https://www.youtube.com/c/InsideOutElectronics/
 */
 #include <bluefruit.h>
 #include "cameraslider.h"
@@ -17,6 +19,13 @@ const int downPin =  11;
 const int leftPin =  13;
 const int rightPin =  12;
 
+static const int NOT_CONNECTED_COLOR = 0xFF0000;
+static const int CONNECTED_COLOR = 0x0000FF;
+static const int MOVING_COLOR = 0x00FF00;
+
+// stealthMode = true fo no LED indication
+static const boolean stealthMode = false;
+
 enum MoveValues {
   TILT_UP,
   TILT_DOWN,
@@ -26,6 +35,14 @@ enum MoveValues {
   TIGGER_ON,
   TIGGER_OFF
 };
+
+enum HeadStatus {
+  NOT_CONNECTED,
+  CONNECTED,      // Or Idle, waiting for commands
+  MOVING
+};
+
+int currentHeadStatus = NOT_CONNECTED;
 
 // OTA DFU service
 BLEDfu bledfu;
@@ -40,11 +57,11 @@ extern uint8_t packetbuffer[];
  */
 void moveHead(int direction) {  
   switch(direction) {
-    case TILT_UP:        digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,HIGH); digitalWrite(downPin,LOW); break;
-    case TILT_DOWN:      digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,HIGH); break;
-    case PAN_LEFT:       digitalWrite(leftPin,HIGH); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
-    case PAN_RIGHT:      digitalWrite(leftPin,LOW); digitalWrite(rightPin,HIGH); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
-    case STOP:           digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
+    case TILT_UP:        setHeadStatus(MOVING); digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,HIGH); digitalWrite(downPin,LOW); break;
+    case TILT_DOWN:      setHeadStatus(MOVING); digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,HIGH); break;
+    case PAN_LEFT:       setHeadStatus(MOVING); digitalWrite(leftPin,HIGH); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
+    case PAN_RIGHT:      setHeadStatus(MOVING); digitalWrite(leftPin,LOW); digitalWrite(rightPin,HIGH); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
+    case STOP:           setHeadStatus(CONNECTED); digitalWrite(leftPin,LOW); digitalWrite(rightPin,LOW); digitalWrite(upPin,LOW); digitalWrite(downPin,LOW); break;
   }
 }
 
@@ -55,6 +72,7 @@ void moveHead(int direction) {
 void connect_callback(uint16_t conn_handle)
 {
   Serial.println("Connected");
+  setHeadStatus(CONNECTED);
 }
 
 /**
@@ -68,6 +86,7 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
   (void) reason;
   Serial.print("Disconnected, reason = 0x"); Serial.println(reason, HEX);
   moveHead(STOP);
+  setHeadStatus(NOT_CONNECTED);
 }
 
 /**
@@ -90,6 +109,21 @@ void startAdv(void)
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+}
+
+void setHeadStatus(HeadStatus headStatus) {
+
+  currentHeadStatus = headStatus;
+  
+  // Provide indication of the status
+  if(!stealthMode){
+    Serial.print ("Head Status: "); Serial.println(currentHeadStatus, HEX);
+    switch(currentHeadStatus) {
+      case NOT_CONNECTED: strip.setPixelColor(0, NOT_CONNECTED_COLOR); strip.show(); break;
+      case CONNECTED: strip.setPixelColor(0, CONNECTED_COLOR); strip.show(); break;
+      case MOVING: strip.setPixelColor(0, MOVING_COLOR); strip.show(); break;
+    }
+  }
 }
 
 void setup() {
@@ -117,9 +151,8 @@ void setup() {
 
   strip.begin(); // Initialize pins for output
   strip.show();  // Turn all LEDs off ASAP
-
-  // May not be required, currently used for debugging
-  strip.setPixelColor(0, 255, 0, 0); strip.show(); // RED
+  setHeadStatus(NOT_CONNECTED);
+  strip.setPixelColor(0, NOT_CONNECTED_COLOR); strip.show();
 }
 
 void loop() {
@@ -130,5 +163,5 @@ void loop() {
     char command = packetbuffer[0];
     Serial.print ("Command "); Serial.println(command, HEX);
     moveHead(command);
-  }    
+  } 
 }
